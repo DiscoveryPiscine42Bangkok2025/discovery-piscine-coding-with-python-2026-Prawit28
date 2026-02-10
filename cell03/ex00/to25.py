e36d1267324e0d61af9e131b9eef041b490b432c
BigMadLotus <prawit.25449@gmail.com>
2026-02-11 01:34:49 +0700

    cell03

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,5 @@
+print("Enter a number less than 25: ")
+num1 = int(input())
+while num1 <= 25:
+    print("Inside the loop, my variable is", num1)
+    num1 += 1
---
+++ cell03/ex01/multiplication_table.py
@@ -0,0 +1,4 @@
+print("Enter a number: ")
+num1 = int(input())
+for i in range(0, 10):
+    print(i, "x", num1, "=", i * num1)
