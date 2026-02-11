5ad8f51d8fd7679916d0c642739b2c89efb1c30b
BigMadLotus <prawit.25449@gmail.com>
2026-02-12 00:20:12 +0700

    edit cell05-14

--- cell05/ex14/free_range.py
+++ cell05/ex14/free_range.py
@@ -5,9 +5,6 @@ params = sys.argv[1:]
 if len(params) == 2:
     start = int(params[0])
     end = int(params[1])
-    arr = []
-    for i in range(start, end + 1):
-        arr.append(i)
-    print(arr)
+    print(list(range(start, end + 1)))
 else:
     print("none")  
